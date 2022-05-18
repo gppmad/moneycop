e0a18dfc2a4b0942dbe49007f453f185eff657b1
Giuseppe Maddiona <giuseppe.maddiona@gmail.com>
2022-05-18 16:09:26 +0200

    tested storage

--- moneycop/storage.py
+++ moneycop/storage.py
@@ -2,5 +2,5 @@ import datetime
 from typing import List
 
 class Storage():
-    def save(self, amount: int, location: str, date: datetime) -> List:
+    def save(self, amount: float, location: str, date: datetime) -> List:
         raise NotImplementedError 
